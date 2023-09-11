4d2cf9b6afad5b6693a3b0d259681afd93d407de
백여우 <95093193+BackFoxx@users.noreply.github.com>
2023-09-11 15:24:21 +0900

    백엔드 프로젝트 initializing

---
+++ backend/src/main/java/edonymyeon/trafficmaker/TrafficmakerApplication.java
@@ -0,0 +1,13 @@
+package edonymyeon.trafficmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficmakerApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/edonymyeon/trafficmaker/task/RegistrationTask.java
@@ -0,0 +1,10 @@
+package edonymyeon.trafficmaker.task;
+
+import java.util.Map;
+
+public class RegistrationTask extends Task {
+    @Override
+    public Map<String, Object> execute(final Map<String, Object> resource) {
+        return null;
+    }
+}
