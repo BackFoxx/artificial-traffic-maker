e889597ba114059896890b0e7786f2867be19729
백여우 <95093193+BackFoxx@users.noreply.github.com>
2023-09-12 13:43:03 +0900

    Feat/#2 회원가입 테스크 (#3)
    * feat: 회원가입 테스크 구현하기
    
    * refactor: 메서드 분리를 통한 리팩토링

--- backend/src/test/java/edonymyeon/trafficmaker/task/TaskTest.java
+++ backend/src/test/java/edonymyeon/trafficmaker/task/TaskTest.java
@@ -1,15 +1,5 @@
 package edonymyeon.trafficmaker.task;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 class TaskTest {
-    @Test
-    @DisplayName("회원가입 작업을 수행하는 테스크를 정의할 수 있다.")
-    void taskDefine1() {
-        final Task task = TaskHolder.getTask("REGISTRATION");
-        assertThat(task).isInstanceOf(RegistrationTask.class);
-    }
+
 }
