777aa4e46b25dc591eba0c4420af2dab62b42d8f
BackFoxx <backfoxxx@gmail.com>
2023-09-18 16:42:57 +0900

    feat: ChatGpt를 사용한 대화 API 구현하기

--- backend/src/test/java/edonymyeon/trafficmaker/task/LoginTaskTest.java
+++ backend/src/test/java/edonymyeon/trafficmaker/task/LoginTaskTest.java
@@ -3,13 +3,15 @@ package edonymyeon.trafficmaker.task;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 class LoginTaskTest {
 
     @Test
-    void 회원가입한_정보를_가지고_로그인_작업을_실행한다() {
+    @DisplayName("회원가입한 정보를 가지고 로그인 작업을 실행한다")
+    void loginTask() {
         final Task task = TaskHolder.getTask("LOGIN");
 
         final Map<String, Object> resource = Map.of(
